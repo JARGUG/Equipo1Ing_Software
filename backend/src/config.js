b96e4b7e17c90b18cdcbe1f99e90a5f3c9ab90f9
AlejandroRG <garcia4312@hotmail.com>
2024-05-08 17:45:00 -0600

    merge main -> mi rama

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 
 module.exports = {
     app: {
-        port: process.env.PORT || 4000
+        port: process.env.PORT || 8889
     },
     mysql: {
-        host: process.env.MYSQL_HOST || 'localhost:4000',
+        host: process.env.MYSQL_HOST || 'localhost:8889',
         user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || '1234',
+        password: process.env.MYSQL_PASSWORD || 'root',
         database: process.env.MYSQL_DB || 'raites'
     }
 }
